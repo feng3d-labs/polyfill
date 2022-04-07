40f11ba54e759ae38024a0901eb65fb77eb81ed7
feng <908087098@qq.com>
2022-04-07 10:12:44 +0800

    1

---
+++ test/ClassUtils.spec.ts
@@ -0,0 +1,31 @@
+// import { EventEmitter } from '@feng3d/event';
+// import { ok, equal } from 'assert';
+// import { classUtils } from '../src';
+
+// describe('ClassUtils', () =>
+// {
+//     it('getQualifiedClassName', () =>
+//     {
+//         let className: string;
+//         let className = classUtils.getQualifiedClassName(EventEmitter);
+//         equal(className, 'feng3d.EventEmitter');
+
+//         className = classUtils.getQualifiedClassName(true);
+//         equal(className, 'Boolean');
+
+//         className = classUtils.getQualifiedClassName(Boolean);
+//         equal(className, 'Boolean');
+
+//         className = classUtils.getQualifiedClassName('1');
+//         equal(className, 'String');
+
+//         className = classUtils.getQualifiedClassName(String);
+//         equal(className, 'String');
+
+//         className = classUtils.getQualifiedClassName(123);
+//         equal(className, 'Number');
+
+//         className = classUtils.getQualifiedClassName(Number);
+//         equal(className, 'Number');
+//     });
+// });
